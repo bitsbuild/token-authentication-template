4c1b6a049437830224c0fa8e59ae8d419827bf2e
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-26 17:58:48 +0530

    Views Completed

--- tokenauth/user/views.py
+++ tokenauth/user/views.py
@@ -1,1 +1,39 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view,permission_classes
+from user.serializer import UserSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
+@api_view(['POST'])
+def create_user(request):
+    user_data = request.data
+    srlzr = UserSerializer(data=user_data)
+    if srlzr.is_valid():
+        user = srlzr.save()
+        token,_ = Token.objects.get_or_create(user=user)
+        return Response({
+            "Status":"Account Creation Successful",
+            "Username":str(request.data['username']),
+            "Email":str(request.data['email']),
+            "Token":str(token.key)
+        },status=status.HTTP_200_OK)
+    else:
+        return Response({
+            "Status":"Error",
+            "Error":""
+        },status=status.HTTP_400_BAD_REQUEST)
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def delete_user(request):
+    try:
+        request.user.delete()
+        return Response(
+            {
+                "Status":"Account Deletion Successful"
+            },status=status.HTTP_200_OK
+        )
+    except Exception as error:
+        return Response({
+            "Status":"Account Deletion Failed",
+            "Error":str(error)
+            },status=status.HTTP_400_BAD_REQUEST)
