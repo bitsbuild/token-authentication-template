15f954f6e193c1af7d26c2c1e347e5f2dd8a2c58
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-26 17:43:35 +0530

    User App Registered To Project

--- tokenauth/tokenauth/settings.py
+++ tokenauth/tokenauth/settings.py
@@ -10,7 +10,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
+    'user'
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
